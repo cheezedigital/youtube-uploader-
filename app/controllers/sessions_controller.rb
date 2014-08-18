131c37f1fb89c5da799e5512d8a38f014f9a8222
Justin Veirs <justinveirs@gmail.com>
2014-08-18 18:02:08 -0400

    working with omni and setting up users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
   def create
+    auth = request.env['omniauth.auth']
+    user = User.find_or_initialize_by(uid: auth['uid'])
+    user.token = auth['credentials']['token']
+    user.name = auth['info']['name']
+    user.save
+    session[:user_id] = user.id
+    flash[:success] = "Welcome, #{user.name}!"
+    redirect_to new_video_path
     
   end
 end
