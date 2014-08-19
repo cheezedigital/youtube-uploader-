164180b70788355ee7683965d6e06e2b33c5c995
Justin Veirs <justinveirs@gmail.com>
2014-08-19 15:46:11 -0400

    working it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :videos, only: [:new, :index]
+  post '/videos/get_upload_token', to: 'videos#get_upload_token', as: :get_upload_token
+  get '/videos/get_video_uid', to: 'videos#get_video_uid', as: :get_video_uidg
   root to: 'videos#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'sessions#fail'
